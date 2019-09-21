a774b6ec5595154037f4a42ab28454164236dc76
Luke Li <lukegaoli@outlook.com>
2019-09-21 12:04:20 -0500

    Rudo Temp Movement

---
+++ Assets/Rudo/Scripts/Rudo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rudo : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+	private Vector3 moveVelocity;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+    }
+	
+    void Update() {
+		Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		moveVelocity = moveInput.normalized * speed;
+	}
+
+	void FixedUpdate() {
+		rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+	}
+}
